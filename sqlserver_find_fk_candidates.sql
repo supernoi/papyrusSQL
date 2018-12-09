2a4a30f56477eba6a1db8718e76e1945cab0e2ce
Juliano Ribeiro <supernoi@msn.com>
2018-12-09 12:48:15 +0000

    Update alias.

--- sqlserver_find_fk_candidates.sql
+++ sqlserver_find_fk_candidates.sql
@@ -5,21 +5,21 @@
 SELECT qPK.table_name AS TableRef ,
        qPK.column_name AS ColumnRef ,
        qPK.constraint_name AS ConstraintRef ,
-       c.table_name ,
-       c.column_name ,
-       t.constraint_name ,
-       c.is_nullable AS Null
-FROM INFORMATION_SCHEMA.COLUMNS c
-LEFT OUTER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE k ON (c.column_name=k.column_name AND k.table_name=c.table_name)
-LEFT OUTER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS t ON (t.constraint_name=k.constraint_name),
-  (SELECT c.table_name,
-          c.column_name,
-          t.constraint_name
+       qFK.table_name ,
+       qFK.column_name ,
+       tFK.constraint_name ,
+       qFK.is_nullable AS Null
+FROM INFORMATION_SCHEMA.COLUMNS qFK
+LEFT OUTER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE k ON (qFK.column_name=k.column_name AND k.table_name=qFK.table_name)
+LEFT OUTER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS t ON (tFK.constraint_name=k.constraint_name),
+  (SELECT qFK.table_name,
+          qFK.column_name,
+          tFK.constraint_name
    FROM INFORMATION_SCHEMA.COLUMNS c
-   LEFT OUTER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE k ON (c.column_name=k.column_name AND k.table_name=c.table_name)
-   LEFT OUTER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS t ON (t.constraint_name=k.constraint_name)
-   WHERE t.constraint_type='PRIMARY KEY') AS qPK
-WHERE (qPK.table_name!=c.table_name
-       AND c.column_name=qPK.column_name)
+   LEFT OUTER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE k ON (qFK.column_name=k.column_name AND k.table_name=qFK.table_name)
+   LEFT OUTER JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS t ON (tFK.constraint_name=k.constraint_name)
+   WHERE tFK.constraint_type='PRIMARY KEY') AS qPK
+WHERE (qPK.table_name!=qFK.table_name
+       AND qFK.column_name=qPK.column_name)
 ORDER BY qPK.table_name,
          qPK.column_name;
